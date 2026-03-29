6fff2eb2c858e0ec2a4c43fde605fd077a2b081e
AdityaScripter99 <arpitaaditya1234@gmail.com>
2026-03-29 09:12:02 +0530

    Major Updates

---
+++ src/constant/__init__.py
@@ -0,0 +1,16 @@
+import os
+
+
+AWS_S3_BUCKET_NAME = "wafer-fault"
+MONGO_DATABASE_NAME = "aditech"
+MONGO_COLLECTION_NAME = "waferfault"
+
+
+TARGET_COLUMN = "quality"
+MONGO_DB_URL = "mongodb+srv://aditya:bxspv9wISMv2zkV1@cluster0.ziyrllc.mongodb.net/?appName=Cluster0"
+
+
+MODEL_FILE_NAME = "model"
+MODEL_FILE_EXTENSION = ".pkl"
+
+artifact_folder = "artifacts"
